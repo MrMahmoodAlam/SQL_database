02d357376f2ceac3591a0b269534839a1f1ab581
mahmood alam <mahmoodalamjh@gmail.com>
2023-09-04 16:39:54 +0530

    added some text

--- src/Components/Database.js
+++ src/Components/Database.js
@@ -12,11 +12,11 @@ export default function Database() {
                     <div className="relative flex flex-col md:flex-row mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
                         
                         <div className="sm:max-w-lg md:pt-16" id="data_text">
-                            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl overflow-hidden">Database design and details</h1>
+                            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl overflow-hidden">SQL Database</h1>
 
-                            <p className="mt-4 text-xl text-gray-500">The GitHub readme contains code and database details. To download the database image, use the provided download button.</p>
+                            <p className="mt-4 text-xl text-gray-500">GitHub contains code and database details, You will get complete details about database inside readme file. To download the database image and code, use the provided download button.</p>
 
-                            <a href={database_files} className="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700 mr-4 mt-10"  download="DatabaseCode">Download code</a>
+                            <a href={database_files} id="download_code" className="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700 mr-4 mt-10"  download="DatabaseCode">Download code</a>
 
                             <a href={database_image} className="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700" download="databaseImage">Download design</a>
                         </div>
